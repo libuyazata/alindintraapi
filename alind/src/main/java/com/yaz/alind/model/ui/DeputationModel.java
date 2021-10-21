76fd5b095d1d862cf7c261dce6e3e45f2127bee7
School <school@yazataconsulting.com>
2021-10-21 17:10:47 +0530

    DeputationModel

---
+++ alind/src/main/java/com/yaz/alind/model/ui/DeputationModel.java
@@ -0,0 +1,127 @@
+package com.yaz.alind.model.ui;
+
+
+public class DeputationModel {
+
+	private int deputationId;
+
+	private int employeeId;
+
+	private String employeeName;
+	
+	private String empCode;
+
+	private int departmentId;
+
+	private String departmentName;
+
+	private String description;
+
+	private String startDate;
+
+	private String endDate;	
+
+	private String createdOn;
+
+	private String updatedOn;
+
+	private int status;
+
+	public int getDeputationId() {
+		return deputationId;
+	}
+
+	public void setDeputationId(int deputationId) {
+		this.deputationId = deputationId;
+	}
+
+	public int getEmployeeId() {
+		return employeeId;
+	}
+
+	public void setEmployeeId(int employeeId) {
+		this.employeeId = employeeId;
+	}
+
+	public String getEmployeeName() {
+		return employeeName;
+	}
+
+	public void setEmployeeName(String employeeName) {
+		this.employeeName = employeeName;
+	}
+
+	public String getEmpCode() {
+		return empCode;
+	}
+
+	public void setEmpCode(String empCode) {
+		this.empCode = empCode;
+	}
+
+	public int getDepartmentId() {
+		return departmentId;
+	}
+
+	public void setDepartmentId(int departmentId) {
+		this.departmentId = departmentId;
+	}
+
+	public String getDepartmentName() {
+		return departmentName;
+	}
+
+	public void setDepartmentName(String departmentName) {
+		this.departmentName = departmentName;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(String startDate) {
+		this.startDate = startDate;
+	}
+
+	public String getEndDate() {
+		return endDate;
+	}
+
+	public void setEndDate(String endDate) {
+		this.endDate = endDate;
+	}
+
+	public String getCreatedOn() {
+		return createdOn;
+	}
+
+	public void setCreatedOn(String createdOn) {
+		this.createdOn = createdOn;
+	}
+
+	public String getUpdatedOn() {
+		return updatedOn;
+	}
+
+	public void setUpdatedOn(String updatedOn) {
+		this.updatedOn = updatedOn;
+	}
+
+	public int getStatus() {
+		return status;
+	}
+
+	public void setStatus(int status) {
+		this.status = status;
+	}
+	
+	
+}
